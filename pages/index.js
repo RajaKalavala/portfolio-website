61534330ad5212f3c1f8c3bc057e12c3c012bcd5
Raja <rajakalavala55@gmail.com>
2022-08-08 20:10:30 +0530

    Add Navbar view

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="font-bold text-5xl">Hello World!</h1>{" "}
-    </div>
-  );
+  return <div></div>;
 }
