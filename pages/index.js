94c1620b2f00ab2a976d7f9064f9073694780eb0
Raja <rajakalavala55@gmail.com>
2022-08-09 20:52:39 +0530

    [Portfolio] Create portfolio component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import HomePage from "../components/HomePage";
+import Portfolio from "../components/Portfolio";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <Portfolio />
     </div>
   );
 }
