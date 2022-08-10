66d0fb0f9478ec5aaead76c4b8b3ad3f1e9474fa
Raja <rajakalavala55@gmail.com>
2022-08-10 20:54:47 +0530

    [Navbar] On scroll make the navebar black

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 import {
@@ -12,6 +12,15 @@ import {
 
 const Navbar = () => {
   const [navigation, setNavigation] = useState(false);
+  const [pageScroll, setPageScroll] = useState(false);
+
+  useEffect(() => {
+    const sub = window.addEventListener("scroll", () =>
+      setPageScroll(window.scrollY >= 90)
+    );
+
+    return sub;
+  });
 
   const links = [
     {
@@ -36,7 +45,11 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in">
+    <div
+      className={`w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in ${
+        pageScroll && "bg-black text-[#fff]"
+      }`}
+    >
       <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4">
         <Link href="/#home">
           <h1 className="text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">
@@ -83,7 +96,10 @@ const Navbar = () => {
           <div>
             <div className="flex w-full items-center justify-between">
               <Link href="/#home">
-                <h2 className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">
+                <h2
+                  onClick={() => setNavigation(false)}
+                  className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer"
+                >
                   Raja
                 </h2>
               </Link>
