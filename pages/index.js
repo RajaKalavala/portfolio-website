77e30ca26fd4cba26a0cd7ae7e839a57e0b534c8
Raja <rajakalavala55@gmail.com>
2022-08-10 20:33:51 +0530

    [Me] Create Me tab

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Experience from "../components/Experience";
 import HomePage from "../components/HomePage";
+import Me from "../components/Me";
 import Portfolio from "../components/Portfolio";
 import styles from "../styles/Home.module.css";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <HomePage />
       <Portfolio />
       <Experience />
+      <Me />
     </div>
   );
 }
